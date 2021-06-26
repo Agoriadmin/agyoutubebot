93c34a03c6568a97176c9b108ca8d5aa0d662980
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-06-26 14:10:18 +0530

    Update and rename help.py to help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Currently Only supports Youtube Single  (No playlist) ചുമ്മാ ഒരു Youtube Url താടോ.."
     await message.reply_text(helptxt)
